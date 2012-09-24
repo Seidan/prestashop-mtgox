dd87a9a37b4fbe6fe4c459d8c34c40232d2a9e08
ludovicbarreca <ludogp2@gmail.com>
2012-09-24 17:38:44 +0200

    Updating controllers & front to manage configuration updates

--- mtgox.php
+++ mtgox.php
@@ -26,6 +26,7 @@ class Mtgox extends PaymentModule
                 !Configuration::updateValue('MTGOX_MERCHANT_ID', '0') OR
                 !Configuration::updateValue('MTGOX_API_KEY', '0') OR
                 !Configuration::updateValue('MTGOX_API_SECRET_KEY', '0') OR
+                !Configuration::updateValue('MTGOX_PAYMENT_DESCRIPTION', 'MtGox Payment Gateway') OR
                 !Configuration::updateValue('MTGOX_EMAIL_ON_SUCCESS', '1') OR
                 !Configuration::updateValue('MTGOX_AUTOSELL', '1') OR
                 !Configuration::updateValue('MTGOX_INSTANT_ONLY', '0'))
@@ -39,9 +40,63 @@ class Mtgox extends PaymentModule
             Configuration::deleteByName('MTGOX_MERCHANT_ID') AND
             Configuration::deleteByName('MTGOX_API_KEY') AND
             Configuration::deleteByName('MTGOX_API_SECRET_KEY') AND
+            Configuration::deleteByName('MTGOX_PAYMENT_DESCRIPTION') AND
             Configuration::deleteByName('MTGOX_EMAIL_ON_SUCCESS') AND
             Configuration::deleteByName('MTGOX_AUTOSELL') AND
             Configuration::deleteByName('MTGOX_INSTANT_ONLY'));
+    }
+
+    public function getContent()
+    {
+        global $smarty;
+        $errors = array();
+
+        if (Tools::isSubmit('submitMtgox')) {
+            foreach ($this->getConfigFields() as $field) {
+                $field_val = Tools::getValue(strtolower($field['config_name']));
+                if (isset($field['empty']) AND $field['empty'] == false) {
+                    if ($field_val != '0' AND empty($field_val)) {
+                        $errors[] = $this->l($field['display_name'].' field cannot be empty');
+                        continue;
+                    }
+                }
+
+                if (isset($field['boolean']) AND $field['boolean'] == true) {
+                    if (!in_array($field_val, array('0', '1'))) {
+                        $errors[] = $this->l($field['display_name'].' field must be a string containing 0 or 1');
+                        continue;
+                    }
+                }
+
+                Configuration::updateValue($field['config_name'], $field_val);
+            }
+
+            if (!$errors)
+            {
+                // Retro 1.4
+                global $currentIndex;
+
+                $curr_index = Tools::property_exists('AdminController', 'currentIndex') ?
+                    AdminController::$currentIndex : $currentIndex;
+                Tools::redirectAdmin($curr_index.'&configure=mtgox&token='.Tools::safeOutput(Tools::getValue('token')).'&conf=4');
+            }
+        }
+
+        $smarty->assign(array(
+            'displayName'        => $this->displayName,
+            'requestUrl'         => Tools::safeOutput($_SERVER['REQUEST_URI']),
+            'merchantId'         => Tools::safeOutput(Tools::getValue('mtgox_merchant_id', Configuration::get('MTGOX_MERCHANT_ID'))),
+            'apiKey'             => Tools::safeOutput(Tools::getValue('mtgox_api_key', Configuration::get('MTGOX_API_KEY'))),
+            'apiSecretKey'       => Tools::safeOutput(Tools::getValue('mtgox_api_secret_key', Configuration::get('MTGOX_API_SECRET_KEY'))),
+            'paymentDescription' => Tools::safeOutput(Tools::getValue('mtgox_payment_description', Configuration::get('MTGOX_PAYMENT_DESCRIPTION'))),
+            'autosell'           => Tools::safeOutput(Tools::getValue('mtgox_autosell', Configuration::get('MTGOX_AUTOSELL'))),
+            'email'              => Tools::safeOutput(Tools::getValue('mtgox_email_on_success', Configuration::get('MTGOX_EMAIL_ON_SUCCESS'))),
+            'instantonly'        => Tools::safeOutput(Tools::getValue('mtgox_instant_only', Configuration::get('MTGOX_INSTANT_ONLY'))),
+            'submit'             => Tools::isSubmit('submitMtgox'),
+            'errors'             => $errors
+        ));
+
+        return $this->display(__FILE__, 'views/configure.tpl');
     }
 
     public function hookPayment()
@@ -54,8 +109,39 @@ class Mtgox extends PaymentModule
     public function preparePayment($cart)
     {
         global $smarty;
+
         $smarty->assign(array(
             'total' => $cart->getOrderTotal(),
         ));
+    }
+
+    private function getConfigFields()
+    {
+        return array(
+            array('config_name' => 'MTGOX_MERCHANT_ID',
+                  'display_name' => 'Merchant ID',
+                  'empty' => false),
+            array('config_name' => 'MTGOX_API_KEY',
+                  'display_name' => 'API Key',
+                  'empty' => false),
+            array('config_name' => 'MTGOX_API_SECRET_KEY',
+                  'display_name' => 'API Secret Key',
+                  'empty' => false),
+            array('config_name' => 'MTGOX_PAYMENT_DESCRIPTION',
+                  'display_name' => 'Payment Description',
+                  'empty' => false),
+            array('config_name' => 'MTGOX_AUTOSELL',
+                  'display_name' => 'Autosell',
+                  'empty' => false,
+                  'boolean' => true),
+            array('config_name' => 'MTGOX_EMAIL_ON_SUCCESS',
+                  'display_name' => 'Email on Success',
+                  'empty' => false,
+                  'boolean' => true),
+            array('config_name' => 'MTGOX_INSTANT_ONLY',
+                  'display_name' => 'Instant Only',
+                  'empty' => false,
+                  'boolean' => true),
+        );
     }
 }
---
+++ views/configure.tpl
@@ -0,0 +1,65 @@
+{if $submit}
+    {foreach from=$errors item=error}
+        <div class="warning warn"><h3>{$error}</h3></div>
+    {foreachelse}
+    {/foreach}
+{/if}
+
+<h2>{$displayName}</h2>
+    <form action="{$requestUrl}" method="POST">
+        <fieldset>
+        <legend><img src="{$base_dir_ssl}../modules/mtgox/logo.gif" />{l s='Settings' mod='mtgox'}</legend>
+            <label>
+                {l s='Merchant ID' mod='mtgox'}
+            </label>
+            <div class="margin-form">
+                <input type="text" name="mtgox_merchant_id" value="{$merchantId}" style="width: 300px;" />
+            </div>
+            <label>
+                {l s='API key' mod='mtgox'}
+            </label>
+            <div class="margin-form">
+                <input type="text" name="mtgox_api_key" value="{$apiKey}" style="width: 300px;" />
+            </div>
+            <label>
+                {l s='API secret key' mod='mtgox'}
+            </label>
+            <div class="margin-form">
+                <input type="text" name="mtgox_api_secret_key" value="{$apiSecretKey}" style="width: 300px;" />
+            </div>
+            <label>
+                {l s='Payment Description' mod='mtgox'}
+            </label>
+            <div class="margin-form">
+                <input type="text" name="mtgox_payment_description" value="{$paymentDescription}" style="width: 300px;" />
+            </div>
+            <label>
+                {l s='Automatically sell received bitcoins' mod='mtgox'}
+            </label>
+            <div class="margin-form">
+                <select name="mtgox_autosell">
+                    <option value="1"{if $autosell} selected="selected"{/if}>{l s='Yes' mod='mtgox'}</option>
+                    <option value="0"{if !$autosell} selected="selected"{/if}>{l s='No' mod='mtgox'}</option>
+                </select>
+            </div>
+            <label>
+                {l s='Receive email on completed transaction' mod='mtgox'}
+            </label>
+            <div class="margin-form">
+                <select name="mtgox_email_on_success">
+                    <option value="1"{if $email} selected="selected"{/if}>{l s='Yes' mod='mtgox'}</option>
+                    <option value="0"{if !$email} selected="selected"{/if}>{l s='No' mod='mtgox'}</option>
+                </select>
+            </div>
+            <label>
+                {l s='Only allow transactions that will settle instantly' mod='mtgox'}
+            </label>
+            <div class="margin-form">
+                <select name="mtgox_instant_only">
+                    <option value="1"{if $instantonly} selected="selected"{/if}>{l s='Yes' mod='mtgox'}</option>
+                    <option value="0"{if !$instantonly} selected="selected"{/if}>{l s='No' mod='mtgox'}</option>
+                </select>
+            </div>
+            <div class="clear center"><input type="submit" name="submitMtgox" class="button" value="{l s='Save' mod='mtgox'}" /></div>
+        </fieldset>
+    </form>
